ca7b854e5737da71d3a76b8dd72fe7a1deeac1c6
linying02 <sun52492@qq.com>
2024-09-28 09:58:35 +0800

    Add files via upload
    自己写的学习总结，可能仅自己能看懂

---
+++ main.py
@@ -0,0 +1,56 @@
+#数据类型识别和转换
+# name = "***"
+# age = 18
+# home = 12.2
+# print (name)
+# print (age)
+# print(home)
+# print (type(name))
+# print (type(name))
+# print (type(age))
+# print (type(home))
+# print (type(str(age)))
+# print (type(str(home)))
+
+#字符串拼接 数据输入
+# name_1 = "山东科技大学"
+# name_2 = "***"
+# name_3 = "18"
+# name_4 = "202311100411"
+# print("我的名字是%s,来自%s,年龄%s,编号是%s" %(name_2,name_1,name_3,name_4))
+# print(f"我的名字是{name_2},来自{name_1},年龄{name_3},编号是{name_4}")
+
+#if数据输入和判断语句和if__else结构
+# name_5 = int(input())
+# if name_5 < 10:
+#     print ("小")
+# elif name_5 > 10:
+#     print ("大")
+
+#布尔运算和while循环和range函数的使用
+# trage = True
+# i = 100
+# while trage:
+#     i=i-1
+#     if i==50:
+#         trage = False
+#         print ("完成")
+#数据输出的换行、对齐
+# name_6 = "***"
+# name_7 = "山东科技大学"
+# print(name_6, end = "")
+# print(name_7)
+# print("efvd\tworld")
+# print("dhrdjf\tbest")
+
+#For循环输出9*9乘法表以及range函数的使用
+# a = 1
+# b = 1
+# for a in range(1,10):
+#     for b in range(1,a+1):
+#         if b < a:
+#             print("%d * %d=%2d  " % (b, a, a * b), end="")
+#         else:
+#             print("%d * %d=%2d  " % (b, a, a * b))
+
+
